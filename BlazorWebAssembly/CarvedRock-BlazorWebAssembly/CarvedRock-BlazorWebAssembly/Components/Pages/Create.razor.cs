b6b3419eb87e1accb8b0d60f093a738557634adb
Roland Guijt <roland.guijt@gmail.com>
2026-05-10 16:14:04 +0200

    Cosmetic

---
+++ BlazorWebAssembly/CarvedRock-BlazorWebAssembly/CarvedRock-BlazorWebAssembly/Components/Pages/Create.razor.cs
@@ -0,0 +1,55 @@
+﻿using CarvedRock_BlazorServer.Data;
+using Microsoft.AspNetCore.Components;
+
+namespace CarvedRock_BlazorServer.Components.Pages
+{
+    public partial class Create
+    {        
+        [SupplyParameterFromForm]
+        private Product NewProduct { get; set; } = default!;
+
+        private string? _nameValidationError;
+
+        protected override void OnInitialized()
+        {
+            NewProduct ??= new();
+        }
+
+        [Inject]
+        private IProductRepository ProductRepository { get; set; } = default!;
+
+        [Inject]
+        private NavigationManager NavigationManager { get; set; } = default!;
+
+        private async Task OnNameInput(ChangeEventArgs e)
+        {
+            var name = e.Value?.ToString();
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                _nameValidationError = null;
+                return;
+            }
+
+            var products = await ProductRepository.GetAll();
+            if (products.Any(p => p.Name.Equals(name, StringComparison.OrdinalIgnoreCase)))
+            {
+                _nameValidationError = "Name already taken";
+            }
+            else
+            {
+                _nameValidationError = null;
+            }
+        }
+
+        private async Task CreateProduct()
+        {
+            if (!string.IsNullOrEmpty(_nameValidationError))
+            {
+                return;
+            }
+
+            await ProductRepository.Add(NewProduct);
+            NavigationManager.NavigateTo("/");
+        }
+    }
+}
